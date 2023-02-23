4dfba5d724da4f5c2ab6e956f325222f18a1978b
BoynChen <chankaser@gmail.com>
2023-02-23 22:18:52 +0800

    feat: add binance

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const Binance = require("node-binance-api");
+const binance = new Binance().options({
+    APIKEY: process.env.API_KEY,
+    APISECRET: process.env.API_SECRET,
+});
+async function main() {
+    console.info(await binance.futuresPrices());
+}
+
+main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
